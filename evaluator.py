4640fbff8bfb8a87995f45a404a0bdd32ba1ae5b
vetniucck <exonumia1@gmail.com>
2017-03-10 21:50:06 -0300

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -1,14 +1,20 @@
 def calc(s):
+    # You should really use floats...
+    
     s = str.lower(s)
-    s = s.replace("power","**")
-    s = s.replace("plus","+")
-    s = s.replace("minus","-")
-    s = s.replace("divided by","/")
-    s = s.replace("by","/")
-    s = s.replace("^","**")
+    replace_dict = {"power": "**",
+                    "plus": "+",
+                    "minus": "-",
+                    "divided": "/", # Changed this keyword cause dicts are not ordered
+                    "by": "*",
+                    "^": "**"}
+    
+    for key in replace_dict.keys():
+        s = s.replace(key, replace_dict[key])
+    
     try:
+        # This is not good, look at ast.literaleval() for a safer way to do this
         x = eval(s)
         print(x)
-    except Exception:
+    except Exception: # This is not good either, shit to debug
         print("Error : Not in correct format")
-
